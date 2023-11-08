e86a32ecfc7d996b63283427d06e6f1b31cf1d42
Corey Unruh <113541726+CUnruh641@users.noreply.github.com>
2023-11-08 12:59:43 -0600

    add full POST method

--- controllers/shakers.js
+++ controllers/shakers.js
@@ -1,6 +1,6 @@
 var Shaker = require('../models/shaker');
 
-// List of all Costumes
+//List of All Shakers.
 exports.shaker_list = async function(req, res) {
     try{
         theShakers = await Shaker.find();
@@ -12,28 +12,43 @@ exports.shaker_list = async function(req, res) {
     }
 }
 
-// for a specific Costume.
+//List for a Specific Shaker.
 exports.shaker_detail = function(req, res) {
     res.send('NOT IMPLEMENTED: Shaker Detail: ' + req.params.id);
 }
 
-// Handle Costume create on POST.
-exports.shaker_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Shaker Create POST');
-}
+// Handle Shaker Create on POST.
+exports.shaker_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Shaker();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    document.shaker_type = req.body.shaker_type;
+    document.shaker_size = req.body.shaker_size;
+    document.shaker_cost = req.body.shaker_cost;
+    try{
+        let result = await document.save();
+        res.send(result);
+    }
+    catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
 
-// Handle Costume delete form on DELETE.
+// Handle Shaker Delete Form on DELETE.
 exports.shaker_delete = function(req, res) {
     res.send('NOT IMPLEMENTED: Shaker Delete DELETE ' + req.params.id);
 }
 
-// Handle Costume update form on PUT.
+// Handle Shaker Update Form on PUT.
 exports.shaker_update_put = function(req, res) {
     res.send('NOT IMPLEMENTED: Shaker Update PUT' + req.params.id);
 }
 
 // VIEWS
-// Handle a show all view
+// Handle a Show All View.
 exports.shaker_view_all_Page = async function(req, res) {
     try{
         theShakers = await Shaker.find();
