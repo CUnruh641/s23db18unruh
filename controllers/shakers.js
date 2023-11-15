76d0b2c8e65dda4159ca28b596cd36821b74bfa5
Corey Unruh <113541726+CUnruh641@users.noreply.github.com>
2023-11-15 14:55:46 -0600

    create the view for one shaker

--- controllers/shakers.js
+++ controllers/shakers.js
@@ -89,3 +89,16 @@ exports.shaker_view_all_Page = async function(req, res) {
         res.send(`{"error": ${err}}`);
     }
 }
+
+// Handle a show one view with id specified by query
+exports.shaker_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+        result = await Shaker.findById( req.query.id)
+        res.render('shakerdetail', {title: 'Shaker Detail', toShow: result});
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+}
--- routes/shaker.js
+++ routes/shaker.js
@@ -5,4 +5,10 @@ var router = express.Router();
 /* GET shakers*/
 router.get('/', shaker_controller.shaker_view_all_Page);
 
+/* GET detail costume page */
+router.get('/detail', shaker_controller.shaker_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', costume_controlers.costume_create_Page);
+
 module.exports = router;
