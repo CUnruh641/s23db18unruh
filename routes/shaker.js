d33d432cdb920d55a4b98082081eb0a3344426ea
Unruh <S543211@nwmissouri.edu>
2023-11-29 19:58:45 -0600

    add authenticator to collection page

--- routes/shaker.js
+++ routes/shaker.js
@@ -1,6 +1,13 @@
 var express = require('express');
 const shaker_controller = require('../controllers/shakers');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on or redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 /* GET shakers*/
 router.get('/', shaker_controller.shaker_view_all_Page);
@@ -12,7 +19,7 @@ router.get('/detail', shaker_controller.shaker_view_one_Page);
 router.get('/create', shaker_controller.shaker_create_Page);
 
 /* GET create update page */
-router.get('/update', shaker_controller.shaker_update_Page);
+router.get('/update', secured, shaker_controller.shaker_update_Page);
 
 /* GET delete costume page */
 router.get('/delete', shaker_controller.shaker_delete_Page);
